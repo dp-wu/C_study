d46ea1ac17cf79de4956e2afb7dd281bf4986ed2
Yves <37640312+dp-wu@users.noreply.github.com>
2020-10-23 15:21:39 -0700

    Update polynomial.cpp

--- polynomial.cpp
+++ polynomial.cpp
@@ -35,68 +35,55 @@
 
 
 // 这段代码我没注释因为前前后后删掉重写了好几遍到最后真的没心情注释了。大家凑合看吧。
-#include <stdio.h>
+ #include <stdio.h>
 
-void get_input(int *arr) {
+ void get_input(int *arr) {
     int n1, n2;
     do {
         scanf("%d%d", &n1, &n2);
         arr[n1] = n2;
     } while (n1 != 0);
-}
+ }
 
-int main() {
-    int p1[101] = {0}, p2[101] = {0}, p[101] = {0};
+ int main() {
+     int p1[101] = {0}, p2[101] = {0}, p[101] = {0};
+     
+     get_input(p1);
+     get_input(p2);
+     
+     int start = 0, ind = 0;
+     for (int i=100; i>=0; --i) {
+         p[i] = p1[i] + p2[i];
+         if (start == 0 && p[i] != 0) {
+             start = 1;
+             ind = i;
+             continue;
+         }
+     }
+     
+     if (p[ind] > 1 || p[ind] < -1) printf("%dx%d", p[ind], ind);
+     else if (p[ind] == 1) printf("x%d", ind);
+     else if (p[ind] == -1) printf("-x%d", ind);
+     
+     if (ind == 0) printf("%d", p[ind]);
+     
+     if (ind > 1) {
+         for (int i=ind-1; i>1; --i) {
+             if (p[i] > 1) printf("+%dx%d", p[i], i);
+             else if (p[i] == 1) printf("+x%d", i);
+             else if (p[i] == -1) printf("-x%d", i);
+             else if (p[i] < -1) printf("%dx%d", p[i], i);
+         }
+         
+         if (p[1] > 1) printf("+%dx", p[1]);
+         else if (p[1] < -1) printf("%dx", p[1]);
+         else if (p[1] == 1) printf("+x");
+         else if (p[1] == -1) printf("-x");
+         
+         if (p[0] >= 0) printf("+%d", p[0]);
+         else printf("%d", p[0]);
+     }
+     
 
-    get_input(p1);
-    get_input(p2);
-    
-    
-    
-    int start = 0, ind = 0;
-    for (int i=100; i>=0; --i) {
-        p[i] = p1[i] + p2[i];
-        if (start == 0 && p[i] != 0) {
-            start = 1;
-            ind = i;
-            continue;
-        }
-    }
-    
-    if (p[ind] > 1 || p[ind] < -1) printf("%dx%d", p[ind], ind);
-    else if (p[ind] == 1) printf("x%d", ind);
-    else if (p[ind] == -1) printf("-x%d", ind);
-    
-    if (ind == 0) printf("%d", p[ind]);
-    else if (ind == 1) {
-        if (p[ind] == 0) {
-            if (p[ind-1] == 0) printf("0");
-            else printf("%d", p[ind]);
-        }
-        else {
-            if (p[ind-1] == 0) printf("%dx+0", p[ind]);
-            else if (p[ind-1] > 0) printf("%dx+%d", p[ind], p[ind-1]);
-            else printf("%dx%d", p[ind], p[ind-1]);
-        }
-    }
-    
-    if (ind > 1) {
-        for (int i=ind-1; i>1; --i) {
-            if (p[i] > 1) printf("+%dx%d", p[i], i);
-            else if (p[i] == 1) printf("+x%d", i);
-            else if (p[i] == -1) printf("-x%d", i);
-            else if (p[i] < -1) printf("%dx%d", p[i], i);
-        }
-        
-        if (p[1] > 1) printf("+%dx", p[1]);
-        else if (p[1] < -1) printf("%dx", p[1]);
-        else if (p[1] == 1) printf("+x");
-        else if (p[1] == -1) printf("-x");
-        
-        if (p[0] >= 0) printf("+%d", p[0]);
-        else printf("%d", p[0]);
-    }
-    
-
-  return 0;
-}
+   return 0;
+ }
